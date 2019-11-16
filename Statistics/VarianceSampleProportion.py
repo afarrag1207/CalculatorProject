81596855b8c660f190da54b5a39b45104e26270c
melaniemercado <melmercado89@gmail.com>
2019-11-16 17:02:45 -0500

    Feature: Added Variance of Sample Proportion code

---
+++ Statistics/VarianceSampleProportion.py
@@ -0,0 +1,11 @@
+from Statistics.Sample import Sample
+
+def VarianceOfPopulationProportion(list):
+
+
+    s = 50
+    listx = Sample(list,s)
+
+    c = VarianceOfPopulationProportion(listx)
+
+    return c
