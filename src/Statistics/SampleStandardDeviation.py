d935c6a062ff9ff39f206317c23fe9fcc7e33ac1
Ali Farrag <afds1207@gmail.com>
2019-11-20 13:47:40 -0500

    Fix: fixed imports

--- src/Statistics/SampleStandardDeviation.py
+++ src/Statistics/SampleStandardDeviation.py
@@ -1,23 +1,21 @@
-import math
-
-def sample_mean #for sample mean
-l= len(a)
-randm = 0
-for i in range(l);
-randm = randm + a[i]
-ans = randm /l
-return ans
-
-def sample_std(b) # for sample standard deviation
-t= len(b)
-s_randm = 0
-x = sample_mean(b)
-for j in range (t):
-s_randm= s_randm + (b[j] - x)**2
-s_ans = math.sqrt(1/(t-1)*s_randm)
-return s_ans
-
-
-sample = [1,2,3,4,5]
-std = sample_std(sample)
-print(std)
+import random
+from Calculator.addition import addition
+from Calculator.division import division
+from Calculator.Square import square
+from Calculator.squareroot import squareroot
+from Calculator.subtraction import subtraction
+from Sample import sample
+from Mean import mean
+def ssd(data):
+    total = 0
+    sample =  random.randint(1, len(data))
+    new_sample = sample(data, sample)
+    new_mean = mean(new_sample)
+    for numb in new_sample:
+        result = subtraction(numb, new_mean)
+        sq = square(result)
+        total = addition(total, sq)
+    n = len(new_sample)
+    d = division(subtraction(1, n), total)
+    samp_sd = squareroot(d)
+    return samp_sd
