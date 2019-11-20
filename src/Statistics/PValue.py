93804afe19de300717bb0c264b137a3ecf5e0d2c
Ali Farrag <afds1207@gmail.com>
2019-11-20 13:26:31 -0500

    Fix: fixed PValue file

--- src/Statistics/PValue.py
+++ src/Statistics/PValue.py
@@ -1,17 +1,12 @@
-import math
+from Statistics.Zscore import zscore
 
-def pvalue(a, b, c, d):
+def P_value(data):
+    data = []
+    n = len(data)
+    score = zscore()
+    for index in n:
+        if n == score:
+            data.append(index)
 
-    p = float(a / b)
-    p1 = float(c)
-    n = float(b)
-    f = float(p - p1)
-    y = float(p1 * (1 - p1))
-    y2 = float(y)
-    y3 = float(y2 / n)
-    z = float(f / math.sqrt(y3))
-    norm = d
-    pval = 1 - float(norm)
-
-    return pval
+    return index
 
