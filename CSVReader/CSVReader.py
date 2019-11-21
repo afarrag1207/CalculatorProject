516d759c1ea1656f57c7c986e7b7ae74969dc346
Ali Farrag <afds1207@gmail.com>
2019-11-21 12:48:44 -0500

    Fix: fixed csv

--- CSVReader/CSVReader.py
+++ CSVReader/CSVReader.py
@@ -1,9 +1,10 @@
 import csv
+import sys
 from Fileutilities.absolutepath import absolutepath
 
 
-# def ClassFactory(class_name, dictionary):
-#    return type(class_name, (object,), dictionary)
+def ClassFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
 
 
 class CsvReader:
@@ -12,14 +13,20 @@ class CsvReader:
     def __init__(self, filepath):
         self.data = []
 
-        with open(absolutepath(filepath)) as text_data:
-            csv_data = csv.DictReader(text_data, delimiter=",")
+        try:
+            file = open(absolutepath(filepath))
+        except OSError:
+            print("Could not open/read file:", filepath)
+            sys.exit()
+
+        with file:
+            csv_data = csv.DictReader(file, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
-        pass
 
-#    def return_data_as_objects(self, class_name):
-#        objects = []
-#        for row in self.data:
-#            objects.append(ClassFactory(class_name, row))
-#        return objects
+
+    def return_data_as_objects(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(ClassFactory(class_name, row))
+        return objects
