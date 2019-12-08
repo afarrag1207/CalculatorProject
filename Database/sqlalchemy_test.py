4006a1249a165753fd2eddc92f1a6a0ce810e6e9
Ali Farrag <afds1207@gmail.com>
2019-12-07 21:55:33 -0500

    feature: inserted more data

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -98,3 +98,19 @@ new_person6 = Person(first_name = 'Tony',
 session.add_all([new_person4, new_person5, new_person6])
 session.commit()
 
+
+# Inserting products
+
+i1 = Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
+i2 = Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
+i3 = Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
+i4 = Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
+i5 = Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
+i6 = Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
+i7 = Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
+i8 = Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
+
+session.add_all([i1, i2, i3, i4, i5, i6, i7, i8])
+session.commit()
+
+
