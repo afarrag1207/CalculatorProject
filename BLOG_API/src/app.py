d9592a528619bbb036cf4636c54f8563afc6bfec
Ali Farrag <afds1207@gmail.com>
2019-12-16 00:30:36 -0500

    Feature: created user and blogpost model

--- BLOG_API/src/app.py
+++ BLOG_API/src/app.py
@@ -1,7 +1,10 @@
 # src/app.py
+
 from flask import Flask
 
 from .config import app_config
+from .models import db, bcrypt
+
 
 
 def create_app(env_name):
@@ -13,6 +16,8 @@ def create_app(env_name):
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
+    bcrypt.init_app(app)
+    db.init_app(app)
 
     @app.route('/', methods=['GET'])
     def index():
