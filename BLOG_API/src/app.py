b905aa97fa9f8e9df33f782d6f808a4436f64343
Ali Farrag <afds1207@gmail.com>
2019-12-20 13:49:51 -0500

    feature: created userview and authentication

--- BLOG_API/src/app.py
+++ BLOG_API/src/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 from .config import app_config
 from .models import db, bcrypt
+from .views.UserView import user_api as user_blueprint # add this line
 
 
 def create_app(env_name):
@@ -19,6 +20,7 @@ def create_app(env_name):
     bcrypt.init_app(app)  # add this line
 
     db.init_app(app)  # add this line
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
 
     @app.route('/', methods=['GET'])
     def index():
