f7353ca2af2c25ff0e06974b7f3ce2e10321ec10
Ali Farrag <afds1207@gmail.com>
2019-12-20 14:02:03 -0500

    task: created postview methods

--- BLOG_API/src/views/BlogpostView.py
+++ BLOG_API/src/views/BlogpostView.py
@@ -24,6 +24,29 @@ def create():
     return custom_response(data, 201)
 
 
+# add this function
+@blogpost_api.route('/', methods=['GET'])
+def get_all():
+    """
+  Get All Blogposts
+  """
+    posts = BlogpostModel.get_all_blogposts()
+    data = blogpost_schema.dump(posts, many=True).data
+    return custom_response(data, 200)
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['GET'])
+def get_one(blogpost_id):
+    """
+  Get A Blogpost
+  """
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 200)
+
+
 def custom_response(res, status_code):
     """
   Custom Response Function
@@ -33,3 +56,42 @@ def custom_response(res, status_code):
         response=json.dumps(res),
         status=status_code
     )
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['PUT'])
+@Auth.auth_required
+def update(blogpost_id):
+    """
+    Update A Blogpost
+    """
+    req_data = request.get_json()
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    if data.get('owner_id') != g.user.get('id'):
+        return custom_response({'error': 'permission denied'}, 400)
+
+    data, error = blogpost_schema.load(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+    post.update(data)
+
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 200)
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['DELETE'])
+@Auth.auth_required
+def delete(blogpost_id):
+  """
+  Delete A Blogpost
+  """
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  if data.get('owner_id') != g.user.get('id'):
+    return custom_response({'error': 'permission denied'}, 400)
+
+  post.delete()
+  return custom_response({'message': 'deleted'}, 204)
