e10432108f4191746253a1a9630a4b452eb7e29e
Ali Farrag <afds1207@gmail.com>
2019-12-20 14:09:24 -0500

    Feature: created test

--- BLOG_API/src/config.py
+++ BLOG_API/src/config.py
@@ -23,7 +23,18 @@ class Production(object):
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 
+class Testing(object):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 app_config = {
     'development': Development,
     'production': Production,
+    'testing': Testing
 }
